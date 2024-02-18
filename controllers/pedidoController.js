305a0c317c8b8c3ac6f08144b43ff5fbea958606
Alejandro Muñoz García <alejandromgarcia90@gmail.com>
2024-02-18 02:01:38 +0100

    commit

--- controllers/pedidoController.js
+++ controllers/pedidoController.js
@@ -1,118 +1,182 @@
-const stripeservice=require('../servicios/servicioStripe');
-const servicioPaypal = require('../servicios/servicioPaypal');
-const {initializeApp}= require('firebase/app');
+const stripeservice = require("../servicios/servicioStripe");
+const servicioPaypal = require("../servicios/servicioPaypal");
+const { initializeApp } = require("firebase/app");
 const app = initializeApp(JSON.parse(process.env.FIREBASE_CONFIG));
 //-----------------CONFIGURACION DE ACCESO: FIREBASE AUTHENTICATION-----------------
-const {getAuth, } = require('firebase/auth');
+const { getAuth } = require("firebase/auth");
 const auth = getAuth(app); // <--- Servicio de acceso a Firebase Authentication
 
 //-----------------CONFIGURACION DE ACCESO: FIRESTORE DATABASE-----------------
-const {getFirestore, getDocs, collection,query, where, setDoc, doc, arrayUnion, updateDoc} = require('firebase/firestore');
+const {
+  getFirestore,
+  getDocs,
+  collection,
+  query,
+  where,
+  setDoc,
+  doc,
+  arrayUnion,
+  updateDoc,
+  getDoc,
+} = require("firebase/firestore");
 
 const db = getFirestore(app); // <--- Servicio de acceso a todas las colecciones de la base de datos de Firestore Database
 
+module.exports = {
+  recuperarProvincias: async (req, res, next) => {
+    try {
+      const provincias = await getDocs(collection(db, "provincias"));
+      let _provincias = [];
+      provincias.forEach((doc) => {
+        _provincias.push(doc.data());
+      });
+      res.status(200).send(_provincias);
+    } catch (error) {
+      console.log("error al recuperar provincias...", error);
+      res.status(500).send([]);
+    }
+  },
+  recuperarMunicipios: async (req, res, next) => {
+    try {
+      const municipios = await getDocs(
+        query(
+          collection(db, "municipios"),
+          where("CPRO", "==", req.params.cpro)
+        )
+      );
+      let _municipios = [];
+      municipios.forEach((doc) => {
+        _municipios.push(doc.data());
+      });
+      res.status(200).send(_municipios);
+    } catch (error) {
+      console.log("error al recuperar municipios...", error);
+      res.status(500).send([]);
+    }
+  },
+  FinalizarPedido: async (req, res, next) => {
+    try {
+      console.log("Datos del pedido...", req.body);
+      let _pedidoInsert = {
+        idPedido: req.body.pedido.idPedido,
+        fechaPedido: req.body.pedido.fechaPedido,
+        estadoPedido: req.body.pedido.estadoPedido,
+        elementosPedido: req.body.pedido.elementosPedido,
+        subtotalPedido: req.body.pedido.subtotalPedido,
+        gastosEnvioPedido: req.body.pedido.gastosEnvioPedido,
+        totalPedido: req.body.pedido.totalPedido,
+        datosPago: {
+          tipodireccionenvio: req.body.pedido.datosPago.tipodireccionenvio,
+          direccionEnvio: req.body.pedido.datosPago.direccionEnvio,
+          nombreEnvio: req.body.pedido.datosPago.nombreEnvio,
+          apellidosEnvio: req.body.pedido.datosPago.apellidosEnvio,
+          telefonoEnvio: req.body.pedido.datosPago.telefonoEnvio,
+          emailEnvio: req.body.pedido.datosPago.emailEnvio,
+          metodoPago: req.body.pedido.datosPago.metodoPago,
+        },
+      };
+      console.log("pedido insertar en cliente...", _pedidoInsert);
+      //let _resultInsert = await addDoc(collection(db,'pedidos'),_pedidoInsert);
+      //Añadimos el pedido directamente al cliente
+      let _clienteSnapshot = await getDocs(
+        query(
+          collection(db, "clientes"),
+          where("cuenta.email", "==", req.body.email)
+        )
+      );
+      let _idCliente = _clienteSnapshot.docs[0].id;
+      console.log("idCliente: ", _idCliente);
+      //Añadimos el pedido directamente al cliente
+      let docRef = doc(db, "clientes", _idCliente);
+      let _resultInsert = await updateDoc(docRef, {
+        pedidosCliente: arrayUnion(_pedidoInsert),
+      });
+      if (_pedidoInsert.datosPago.metodoPago === "pagopaypal") {
+        let _resp = await servicioPaypal.crearPagoPAYPAL(
+          req.body.email,
+          _pedidoInsert
+        );
+        res.status(200).send({
+          url: JSON.stringify({ urlPayPal: _resp }),
+        });
+      }
+    } catch (error) {
+      console.log("error al finalizar pedido...", error);
+      res.status(403).send({
+        codigo: 1,
+        mensaje:
+          "error al finalizar pedido en servicio de nodejs contra firebase",
+        error: error.message,
+        datoscliente: null,
+        tokensesion: null,
+        otrosdatos: null,
+      });
+    }
+  },
+  paypalCallback: async (req, res, next) => {
+    //OJO este servicio es invocado por paypal cuando el cliente ha finalizado el pago
+    //En la url viene parametros:
+    //idcli <--- idcliente que ha hecho el pedido
+    //pedido <--- idpedido que ha hecho el pedido
+    //cancel <--- true o false
 
-module.exports={
-    recuperarProvincias: async (req,res,next)=>{
-        try{
-            const provincias = await getDocs(collection(db,'provincias'));
-            let _provincias = [];
-            provincias.forEach((doc) => {
-                _provincias.push(doc.data());
-            });
-            res.status(200).send(_provincias);
-        }catch(error){
-            console.log('error al recuperar provincias...',error);
-            res.status(500).send([]);
-        }
-    },
-    recuperarMunicipios: async (req,res,next)=>{
-        try{
-            const municipios = await getDocs(query(collection(db,'municipios'),where('CPRO','==',req.params.cpro)));
-            let _municipios = [];
-            municipios.forEach((doc) => {
-                _municipios.push(doc.data());
-            });
-            res.status(200).send(_municipios);
+    console.log("Parametros recibidos...", req.query);
+    let { email, idpedido, cancel } = req.query;
+    //necesito obtener el id-pago generado por paypal para el pedido
+    //Actualizamos el pedido que se encuentra en el cliente
+    let _pagoSnapshot = await getDocs(
+      query(
+        collection(db, "pagosPayPal"),
+        where("idpedido", "==", idpedido),
+        where("idcliente", "==", email)
+      )
+    );
+    console.log("pagoSnapshot...", _pagoSnapshot.docs);
+    let _pago = _pagoSnapshot.docs[0].data();
+    let _finPagoOk = await servicioPaypal.finalizarPagoPAYPAL(_pago.idpago);
 
-        }catch(error){
-            console.log('error al recuperar municipios...',error);
-            res.status(500).send([]);
-        }
-    },
-    FinalizarPedido: async (req,res,next)=>{
-        try {    
-            console.log('Datos del pedido...', req.body);
-            let _pedidoInsert={
-                idPedido: req.body.pedido.idPedido,
-                fechaPedido: req.body.pedido.fechaPedido,
-                estadoPedido: req.body.pedido.estadoPedido,
-                elementosPedido: req.body.pedido.elementosPedido,
-                subtotalPedido: req.body.pedido.subtotalPedido,
-                gastosEnvioPedido: req.body.pedido.gastosEnvioPedido,
-                totalPedido: req.body.pedido.totalPedido,
-                datosPago:{
-                    tipodireccionenvio: req.body.pedido.datosPago.tipodireccionenvio,
-                    direccionEnvio: req.body.pedido.datosPago.direccionEnvio,
-                    nombreEnvio: req.body.pedido.datosPago.nombreEnvio,
-                    apellidosEnvio: req.body.pedido.datosPago.apellidosEnvio,
-                    telefonoEnvio: req.body.pedido.datosPago.telefonoEnvio,
-                    emailEnvio: req.body.pedido.datosPago.emailEnvio,
-                    metodoPago: req.body.pedido.datosPago.metodoPago,
-                }
-            }
-            console.log('pedido insertar en cliente...', _pedidoInsert);
-            //let _resultInsert = await addDoc(collection(db,'pedidos'),_pedidoInsert);  
-            //Añadimos el pedido directamente al cliente
-            let _clienteSnapshot = await getDocs(query(collection(db, 'clientes'),where('cuenta.email','==',req.body.email)));
-            let _idCliente = _clienteSnapshot.docs[0].id;
-            console.log('idCliente: ', _idCliente);
-            //Añadimos el pedido directamente al cliente
-            let docRef = doc(db, 'clientes', _idCliente);
-            let _resultInsert = await updateDoc(docRef, {
-                pedidosCliente: arrayUnion(_pedidoInsert)
-            });
-            if (_pedidoInsert.datosPago.metodoPago === 'pagopaypal') {
-                let _resp = await servicioPaypal.crearPagoPAYPAL(req.body.email, _pedidoInsert);
-                res.status(200).send(
-                    {
-                        url: JSON.stringify({urlPayPal: _resp})
-                    }
-                );
-            }
-        } catch (error) {
-            console.log('error al finalizar pedido...', error);
-            res.status(403).send(
-                {
-                    codigo: 1,
-                    mensaje: 'error al finalizar pedido en servicio de nodejs contra firebase',
-                    error: error.message,
-                    datoscliente: null,
-                    tokensesion:null,
-                    otrosdatos:null
-                }
-            )
-        }
-    },
-    paypalCallback: async (req,res,next)=>{//OJO este servicio es invocado por paypal cuando el cliente ha finalizado el pago
-                //En la url viene parametros: 
-        //idcli <--- idcliente que ha hecho el pedido
-        //pedido <--- idpedido que ha hecho el pedido
-        //cancel <--- true o false
-        
-        console.log('Parametros recibidos...', req.query);
-        let {email,idpedido,cancel}=req.query;
-        //necesito obtener el id-pago generado por paypal para el pedido
-        //Actualizamos el pedido que se encuentra en el cliente
-        let _clienteSnapshot = await getDocs(query(collection(db, 'clientes'),where('cuenta.email','==',req.body.email)));
-        let _idCliente = _clienteSnapshot.docs[0].id;
-        console.log('idCliente: ', _idCliente);
-        if(! _finPagoOk || cancel==true) throw new Error('Error al finalizar pago con PAYPAL');
-        
-        res.status(200).redirect(`http://localhost:4200/Pedido/FinalizarPedidoOk?email=${email}&idpedido=${idpedido}`);
-    },
+    if (!_finPagoOk || cancel == true)
+      throw new Error("Error al finalizar pago con PAYPAL");
+    let _clienteSnapshot = await getDocs(
+      query(collection(db, "clientes"), where("cuenta.email", "==", email))
+    );
+    //Actualizamos el pedido que se encuentra en el cliente
+    let _idCliente = _clienteSnapshot.docs[0].id;
+    let docRef = doc(db, "clientes", _idCliente);
+    // Obtén el documento
+    let docSnapshot = await getDoc(docRef);
 
-    /**
+    // Asegúrate de que el documento exista y tenga un campo pedidosCliente
+    if (
+      docSnapshot.exists &&
+      Array.isArray(docSnapshot.data().pedidosCliente)
+    ) {
+      // Encuentra el índice del pedido que quieres actualizar
+      let index = docSnapshot
+        .data()
+        .pedidosCliente.findIndex((pedido) => pedido.idPedido === idpedido);
+
+      // Si el pedido existe en el array
+      if (index !== -1) {
+        // Crea una copia del array pedidosCliente
+        let pedidosCliente = [...docSnapshot.data().pedidosCliente];
+
+        // Actualiza el estado del pedido
+        pedidosCliente[index].estadoPedido = "pagado";
+
+        // Actualiza el documento con el nuevo array
+        let _resultUpdate = await updateDoc(docRef, { pedidosCliente });
+      }
+    }
+    res
+    //http://localhost:4200/Tienda/PedidoFinalizado?email=alexanderbelmont2103@gmail.com&idpedido=4acb5dd1-deae-4e58-8641-90dc7f6e949f%22
+      .status(200)
+      .redirect(
+        `http://localhost:4200/Tienda/PedidoFinalizado?email=${email}&idpedido=${idpedido}`
+      );
+  },
+
+  /**
       //1º acceder a las claves de desarrollador de la api de paypal
  HttpRequestMessage _requestToken = new HttpRequestMessage(HttpMethod.Post,
                                                            "https://api-m.sandbox.paypal.com/v1/oauth2/token");
@@ -187,4 +251,4 @@ module.exports={
  } 
      
      */
-}
+};
--- controllers/tiendaController.js
+++ controllers/tiendaController.js
@@ -1,89 +1,120 @@
-const {initializeApp}= require('firebase/app');
+const { initializeApp } = require("firebase/app");
 const app = initializeApp(JSON.parse(process.env.FIREBASE_CONFIG));
 //-----------------CONFIGURACION DE ACCESO: FIREBASE AUTHENTICATION-----------------
-const {getAuth} = require('firebase/auth');
+const { getAuth } = require("firebase/auth");
 const auth = getAuth(app); // <--- Servicio de acceso a Firebase Authentication
 
 //-----------------CONFIGURACION DE ACCESO: FIRESTORE DATABASE-----------------
-const {getFirestore, getDocs, collection,query, where} = require('firebase/firestore');
+const {
+  getFirestore,
+  getDocs,
+  collection,
+  query,
+  where,
+} = require("firebase/firestore");
 const db = getFirestore(app); // <--- Servicio de acceso a todas las colecciones de la base de datos de Firestore Database
 
 module.exports = {
-    recuperarCategorias:async function(req,res,next){
-      try {
-        var _idcategoria=req.params.idcategoria;
-        let _categorias = [];
-        var _patron=_idcategoria==='padres' ?  new RegExp("^\\d{1,}$") : new RegExp("^" + _idcategoria + "-\\d{1,}$");
-        const _cats = await getDocs(collection(db,'categorias'));
-        _cats.forEach((doc) => {
-          if(_patron.test(doc.data().IdCategoria)){
-            _categorias.push(doc.data());
-          }
-        });
-        res.status(200).send(_categorias);
+  recuperarCategorias: async function (req, res, next) {
+    try {
+      var _idcategoria = req.params.idcategoria;
+      let _categorias = [];
+      var _patron =
+        _idcategoria === "padres"
+          ? new RegExp("^\\d{1,}$")
+          : new RegExp("^" + _idcategoria + "-\\d{1,}$");
+      const _cats = await getDocs(collection(db, "categorias"));
+      _cats.forEach((doc) => {
+        if (_patron.test(doc.data().IdCategoria)) {
+          _categorias.push(doc.data());
+        }
+      });
+      res.status(200).send(_categorias);
     } catch (error) {
-    console.log('error al recuperar categorias...',error);
-    res.status(500).send( [] );
-    } 
-    },
-    recuperarLibros:async function(req,res,next){
-        try{
-            let _libros = [];
-            //El patron debe ser: empieza por idcategoria y puede contener guiones y mas digitos
-            //idcategoria-2 y también idcategoria-2-3
-            let _patron = new RegExp('^' + req.params.idcategoria + '(-\\d+)*$');
-            
-            //Seleccionamos todos los libros de la coleccion, y luego filtramos por patrón
-            const _librosSnap = await getDocs(collection(db,'libros'));
-            
-            _librosSnap.forEach((doc) => {
-              //Si contiene el patrón, lo añadimos al array de libros
-              if(_patron.test(doc.data().IdCategoria)){
-                _libros.push(doc.data());
-              }
-            });
-            res.status(200).send(_libros);
-          }catch(error){
-            console.log("Error al enviar libros", error);
-            res.status(500).send([]);
+      console.log("error al recuperar categorias...", error);
+      res.status(500).send([]);
+    }
+  },
+  recuperarLibros: async function (req, res, next) {
+    try {
+      let _libros = [];
+      //El patron debe ser: empieza por idcategoria y puede contener guiones y mas digitos
+      //idcategoria-2 y también idcategoria-2-3
+      let _patron = new RegExp("^" + req.params.idcategoria + "(-\\d+)*$");
+
+      //Seleccionamos todos los libros de la coleccion, y luego filtramos por patrón
+      const _librosSnap = await getDocs(collection(db, "libros"));
+
+      _librosSnap.forEach((doc) => {
+        //Si contiene el patrón, lo añadimos al array de libros
+        if (_patron.test(doc.data().IdCategoria)) {
+          _libros.push(doc.data());
         }
-    },
-    recuperarLibro:async function(req,res,next){
-        try{
-          //Buscamos en la coleccion libros de firestore el libro con el isbn13 recibido
-          const _librosSnap = await getDocs(query(collection(db,'libros'),where('ISBN13','==',req.params.isbn13)));
-          let _libro = {};
-          _librosSnap.forEach((doc) => {
-            _libro = doc.data();
-          });
-          res.status(200).send(_libro);
-        }catch(error){
-          console.log("Error al enviar libro", error);
-          res.status(500).send(error);
-        }
-    },
-    recuperarComentarios:async function(req,res,next){
-        try{
-          
-          }catch(error){
-            console.log("Error al enviar comentarios", error);
-            res.status(500).send([]);
-          }
-    },
-    enviarComentario:async function(req,res,next){
-        try{
-          
-        }catch(error){
-          console.log("Error al enviar comentario", error);
-          res.status(500).send([]);
-        }
-    },
-    editarComentario: async function (req, res, next) {
-        try {
-          
-        } catch (error) {
-          console.log("Error al enviar comentario", error);
-          res.status(500).send([]);
-        }
-      },
+      });
+      res.status(200).send(_libros);
+    } catch (error) {
+      console.log("Error al enviar libros", error);
+      res.status(500).send([]);
+    }
+  },
+  BuscarLibros: async function (req, res, next) {
+    // Crear una referencia a la colección de libros
+    console.log("buscando libros con el patrón:", req.params.busqueda);
+    let busqueda = req.params.busqueda;
+    let librosRef = collection(db, "libros");
+
+    //Cogemos todos los libros de la coleccion, y después filtramos por el patrón
+    const librosSnap = await getDocs(librosRef);
+    let libros = [];
+    librosSnap.forEach((doc) => {
+      if (
+        doc.data().Titulo.toLowerCase().includes(busqueda.toLowerCase()) ||
+        doc.data().Autores.toLowerCase().includes(busqueda.toLowerCase()) ||
+        doc.data().ISBN13.toLowerCase().includes(busqueda.toLowerCase())
+      ) {
+        libros.push(doc.data());
+      }
+    });
+    res.status(200).send(libros);
+  },
+  recuperarLibro: async function (req, res, next) {
+    try {
+      //Buscamos en la coleccion libros de firestore el libro con el isbn13 recibido
+      const _librosSnap = await getDocs(
+        query(
+          collection(db, "libros"),
+          where("ISBN13", "==", req.params.isbn13)
+        )
+      );
+      let _libro = {};
+      _librosSnap.forEach((doc) => {
+        _libro = doc.data();
+      });
+      res.status(200).send(_libro);
+    } catch (error) {
+      console.log("Error al enviar libro", error);
+      res.status(500).send(error);
+    }
+  },
+  recuperarComentarios: async function (req, res, next) {
+    try {
+    } catch (error) {
+      console.log("Error al enviar comentarios", error);
+      res.status(500).send([]);
+    }
+  },
+  enviarComentario: async function (req, res, next) {
+    try {
+    } catch (error) {
+      console.log("Error al enviar comentario", error);
+      res.status(500).send([]);
+    }
+  },
+  editarComentario: async function (req, res, next) {
+    try {
+    } catch (error) {
+      console.log("Error al enviar comentario", error);
+      res.status(500).send([]);
+    }
+  },
 };
